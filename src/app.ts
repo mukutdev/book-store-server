f71f78dfccba6c327d27534592e742e3fdc3a54e
Mukut Chakrabarty <mukutckb1@gmail.com>
2023-07-23 20:12:20 +0600

    routes and not found routes setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import routes from './app/modules/routes';
+import httpStatus from 'http-status';
 
 const app: Application = express();
 
@@ -11,20 +13,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use('/api/v1', routes);
+app.use('/api/v1', routes);
 
-// app.use((req: Request, res: Response) => {
-//   res.status(httpStatus.NOT_FOUND).json({
-//     status: false,
-//     message: 'Not Found',
-//     errorMessages: [
-//       {
-//         path: req.originalUrl,
-//         message: 'API Not Found',
-//       },
-//     ],
-//   });
-// });
+app.use((req: Request, res: Response) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    status: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Book Store working successfully!');
---
+++ src/app/modules/routes/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+
+const router = express.Router();
+// router.use('/admins' , AdminRoute)
+const moduleRoutes = [
+  {
+   
+  },
+  
+];
+
+// moduleRoutes.forEach(route => router.use(route.path, route.route));
+
+export default router;
